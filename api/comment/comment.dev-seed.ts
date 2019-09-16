0daa395dc7f66620dd552c101f3904fcc050f9fc
mkarpov <mkarpov@griddynamics.com>
2019-09-16 17:26:41 +0200

    Fixes

--- api/comment/comment.dev-seed.ts
+++ api/comment/comment.dev-seed.ts
@@ -3,9 +3,9 @@ import { CommentEntity } from './comment.entity';
 
 export class SeedDevComment1568108033825 implements MigrationInterface {
   public async up(_: QueryRunner): Promise<any> {
-    // await getRepository(CommentEntity).save({
-    //   id: 0,
-    // });
+    await getRepository(CommentEntity).save({
+      id: 0,
+    });
   }
 
   public async down(_: QueryRunner): Promise<any> {
